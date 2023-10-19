009aa4d4a127158a6416cc47e7a780024d0dad43
ackarlse <anncharlottkarlsen@ymail.com>
2023-10-19 23:39:17 +0200

    faar 405 method not allowd i console og i postman...

--- next-api/src/app/api/games/route.js
+++ next-api/src/app/api/games/route.js
@@ -35,35 +35,12 @@ const videoGames = [
   }
   export default function handler(req, res) {
     if (req.method === 'POST') {
-          // tar i mot data som sendes med forespørselen
       const data = req.body
-          
-          // legger til data i quiz listen vår
       videoGames.push(data)
-          
-          // sender status 201 (Created) og den nye oppdaterte listen
       res.status(201).json({ success: true, data: videoGames })
     } else {
       res.status(200).json({ success: true, data: videoGames })
     }
   }
   
-  /*export async function POST(req, res) {
-    if (req.method === 'POST') {
-      try {
-        const data = req.body;
-        if (!data?.newGame) {
-          res.status(400).json({ success: false, error: 'Fyll ut all nødvendig data' });
-        } else {
-          videoGames.push(data);
-          res.status(201).json({ success: true, data: videoGames });
-        }
-      } catch (error) {
-        res.status(500).json({ success: false, error: 'Internal Server Error' });
-      }
-    } else if (req.method === 'PUT') {
-      res.status(405).end();
-    } else {
-      res.status(200).json({ success: true, data: videoGames });
-    }
-  }*/
+ 
